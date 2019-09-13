e12d83db6cfabf45d47efcd2073b2ded84335fd0
Aijah Chappell <aijahc@comcast.net>
2019-09-13 17:26:02 -0400

    express-handlebars main.handlebars view file and scraping script in app.js

---
+++ bin/app.js
@@ -0,0 +1,10 @@
+$(() => {
+  $(".scrape").on("click", () => {
+    $.get("/scrape")
+    .done(data => {
+      alert("Articles scraped!");
+      
+      $("body").html(data);
+    });
+  });
+});
